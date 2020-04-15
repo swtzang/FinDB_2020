dd884b83b92069b89c5ed9be5609b6b876da3d0b
swtzang <swtzang@example.com>
2020-04-15 21:28:55 +0800

    0415

---
+++ R02_1.R
@@ -0,0 +1,11 @@
+# Reference: Steph Locke, Data Manipulation in R
+#
+install.packages(c("tidyverse","ggplot2movies",
+                   "nycflights13","odbc",
+                   "writexl", "openxlsx",
+                   "gapminder"))
+
+library(tidyverse, ggplot2movies, nycflights13, openxlsx, gapminder)
+library(writexl)
+
+
